3074d20abe780f0bc2ab6e2189041f905415b44d
Ruslan.Mustakayev <ike4roo@gmail.com>
2023-09-10 11:41:37 +0500

    tasks added

---
+++ 2023.09.09 HW/task10/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int Proc_Message(string message)
+{
+    System.Console.Write(message);
+    string var1 = Console.ReadLine();
+    int var1_conv = Convert.ToInt32(var1);
+    return var1_conv;
+}
+
+int var2 = Proc_Message("Pls, give 3 digit number here:  ");
+if (var2 > 99 && var2 < 1000)
+{
+    int secDig = (var2 / 10) % 10;
+    Console.WriteLine($"Secong digit in number {var2} is {secDig}");
+}
+else
+    Console.WriteLine("Thus, this is not a correct 3 digit number. Repeat.");
+    return 0;
+
+
---
+++ 2023.09.09 HW/task13/Program.cs
@@ -0,0 +1,26 @@
+﻿int proc_Message(string message)
+{
+    Console.Write(message);
+    string var1 = Console.ReadLine();
+    int var1_conv = Convert.ToInt32(value);
+    return var1_conv;
+}
+
+int CheckThree(int var2)
+{
+    if (var2 > 99 && var2 < 1000)
+    {
+        var2_3dig = (var2 / 10) % 10;
+        Console.WriteLine($"The 3d digit in {var2} is {var2_3dig}");
+    }
+    else if (var2 < 100)
+    {
+        Console.WriteLine("The number you've enetred has less than 3 digits");
+    }
+}
+
+int var2 = proc_Message("Pls, give a number here:  ")
+if (CheckThree(var2))
+{
+    Console.WriteLine(CheckThree(var2))
+}
