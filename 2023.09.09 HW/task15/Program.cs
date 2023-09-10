b5f257447523e77e2217a18b6d694d880be0d0c3
Ruslan.Mustakayev <ike4roo@gmail.com>
2023-09-10 11:59:28 +0500

    HW was done

--- 2023.09.09 HW/task15/Program.cs
+++ 2023.09.09 HW/task15/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int proc_Message(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool ValidateWeekDay(int number)
+{
+    if (number > 0 && number <=7)
+    {
+        return true;
+    }
+    Console.WriteLine("This is not a valid number for week");
+    return false;
+}
+
+int weekDay = proc_Message("Give a day number in a week here:  ");
+if (ValidateWeekDay(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("It's a weekend");
+    }
+    else
+    {
+        Console.WriteLine("It's a work day");
+    }
+}
