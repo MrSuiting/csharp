deb3d3d5ca610fc8cb2586f75eefca381a8267fb
Ruslan.Mustakayev <ike4roo@gmail.com>
2023-09-17 10:15:27 +0500

    tasks 25, 27, 29 were added as a homework

---
+++ 2023.09.09 HW/task25/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numA, numB, numR, i = 0;
+
+        Console.WriteLine("Enter numbers \"A\" to be raised to power \"B\"");
+        numA = Convert.ToInt32(Console.ReadLine());
+        numB = Convert.ToInt32(Console.ReadLine());
+
+        if (numB < 1)
+            Console.Write("Incorrect number");
+        else
+        {
+            while (numB != 0)
+            {
+                numR = 1;
+                numR = numR * numA;
+                numB = numB - 1;
+            }
+            Console.WriteLine(numR);
+        }
+
+    }
+}
---
+++ 2023.09.09 HW/task27/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int len = Length(a);
+Console.Write($"Sum of digits in number {a} is: {Sum(a, len)}");
+
+int Length(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+int Sum(int a, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
