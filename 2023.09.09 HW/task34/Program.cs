71900130c03528004713aa5b892518e15ae6f77c
Ruslan.Mustakayev <ike4roo@gmail.com>
2023-09-23 08:33:46 +0500

    tasks 34, 36, 38

---
+++ 2023.09.09 HW/task34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Elements in array. Quantity: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int[] randomArray = new int[a];
+
+        void array(int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                randomArray[i] = new Random().Next(99, 999);
+                Console.Write(randomArray[i] + " ");
+            }
+
+        }
+
+        int quan(int[] randomArray)
+        {
+            int digs = 0;
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                if (randomArray[i] % 2 == 0)
+                    digs = digs + 1;
+            }
+            return digs;
+        }
+
+        array(a);
+        Console.Write($"Quantity of even numbers: {quan(randomArray)}");
+    }
+}
---
+++ 2023.09.09 HW/task36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+
+Console.Write("Quantity of elements in array:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void array(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = new Random().Next(1, 9);
+        Console.WriteLine(randomArray[i] + " ");
+    }
+
+}
+
+int quan(int[] randomArray)
+{
+    int sum = 0;
+    int i = 0;
+    while (i < randomArray.Length)
+    {
+        sum = sum + randomArray[i];
+        i = i + 2;
+    }
+    return sum;
+}
+
+array(a);
+Console.Write($"Elements sum in odd positions : { quan(randomArray)}");
