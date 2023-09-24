fbac0a6d159618013f0edc4dd1f52eeda668fbf9
Ruslan.Mustakayev <ike4roo@gmail.com>
2023-09-24 09:50:28 +0500

    tasks 41 & 43 were added

---
+++ 2023.09.09 HW/task41/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("How much elements should be? Give quantity:  ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int[] Array = new int[m];
+
+        void arr(int m)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine($"Give elements one-by-one: {i + 1} ");
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+
+        int quan(int[] Array)
+        {
+            int i = 0;
+            int sum = 0;
+            while (i < Array.Length)
+            {
+                if (Array[i] > 0)
+                    sum = sum + 1;
+                i = i + 1;
+            }
+            return sum;
+        }
+
+        arr(m);
+        Console.Write($"Quantity of numbers bigger than zero is: { quan(Array)}");
+    }
+}
---
+++ 2023.09.09 HW/task43/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Value b1:  ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Value k1:  ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Value b2:  ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Value k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        double x = -(b1 - b2) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.Write($"Crossing point is: [{x},{y}]");
+    }
+}
